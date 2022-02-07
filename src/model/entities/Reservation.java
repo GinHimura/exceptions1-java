e1bf6668dbbbbeccda9cb56ebfd47b737f49a8d3
Gerson dos Reis de Melo <gerson.jp.otaku@gmail.com>
2022-02-07 16:41:05 -0300

    Good solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,73 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	
+	
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) throws DomainException {
+		if(!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+	
+	
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkin, Date checkout) throws DomainException   {
+		Date nowTime =  new Date();
+		if(checkin.before(nowTime) || checkout.before(nowTime)) {
+			throw new DomainException ("Reservation dates for update must be future dates");
+		} if(!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.checkin = checkin;
+		this.checkout = checkout;	
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Reservation: Room ");
+		sb.append(roomNumber);
+		sb.append(", check-in: ");
+		sb.append(sdf.format(checkin));
+		sb.append(", check-out: ");
+		sb.append(sdf.format(checkout));
+		sb.append(", ");
+		sb.append(duration());
+		sb.append(" nights");
+		return sb.toString();
+	}
+}
